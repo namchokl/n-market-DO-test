288532649662f53795c129a25ae8dc8c5014dd85
Namchok_L <Namchok.Laohapongpun@gmail.com>
2020-10-22 19:18:15 +0700

    32. Cart Reducer & Add To Cart Action.(use localStorage)

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -7,7 +7,7 @@ import {
     PRODUCT_DETAILS_SUCCESS,
     PRODUCT_DETAILS_FAIL
 
-} from '../constants.js/productConstants'
+} from '../constants/productConstants'
 
 export const listProducts = () => async (dispatch) => {
     try {
