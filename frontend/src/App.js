0834b3dbf5ed1999196212ea87717b2026444695
Namchok_L <Namchok.Laohapongpun@gmail.com>
2020-10-23 12:56:58 +0700

    44. User Login Screen & Functionality.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
@@ -6,6 +6,7 @@ import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
 
 const App = () => {
   return (
@@ -13,15 +14,15 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <Route path='/' exact component={HomeScreen} />
+          <Route path='/login' exact component={LoginScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
-
+          <Route path='/' exact component={HomeScreen} />
         </Container>
       </main>
       <Footer />
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
