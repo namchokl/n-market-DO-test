ef35a377866ac35c7b509e04473e21b8ca42b1d8
Namchok_L <Namchok.Laohapongpun@gmail.com>
2020-10-29 00:48:38 +0700

    86. Product Pagination.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,10 +39,24 @@ const App = () => {
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/admin/userlist' component={UserListScreen} />
           <Route path='/admin/user/:id' component={UserEditScreen} />
-          <Route path='/admin/productlist' component={ProductListScreen} />
+          <Route
+            exact
+            path='/admin/productlist'
+            component={ProductListScreen}
+          />
+          <Route
+            exact
+            path='/admin/productlist/:pageNumber'
+            component={ProductListScreen}
+          />
           <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
           <Route path='/admin/orderlist' component={OrderListScreen} />
-          <Route path='/search/:keyword' component={HomeScreen} />
+          <Route exact path='/search/:keyword' component={HomeScreen} />
+          <Route
+            path='/search/:keyword/page/:pageNumber'
+            component={HomeScreen}
+          />
+          <Route path='/page/:pageNumber' component={HomeScreen} />
           <Route path='/' exact component={HomeScreen} />
         </Container>
       </main>
